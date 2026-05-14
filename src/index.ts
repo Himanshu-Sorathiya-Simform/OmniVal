cfec2c121f7fff84e0b1283401f9c3c37d17dc69
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-14 10:34:45 +0530

    add boolean primitive schema

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import { BooleanSchema } from './schemas/BooleanSchema.js';
 import { NumberSchema } from './schemas/NumberSchema.js';
 import { StringSchema } from './schemas/StringSchema.js';
 
 const v = {
 	string: () => new StringSchema(),
 	number: () => new NumberSchema(),
+	boolean: () => new BooleanSchema(),
 };
 
 export { v };
---
+++ src/schemas/BooleanSchema.ts
@@ -0,0 +1,26 @@
+import type { CheckFn, CheckFnReturnType, ValidateFnReturnType } from '../types.js';
+import { BasePrimitiveSchema } from './BasePrimitiveSchema.js';
+
+class BooleanSchema extends BasePrimitiveSchema {
+	protected type: string = 'boolean';
+	protected checks: Array<CheckFn> = [];
+
+	protected override validateType(data: any): CheckFnReturnType {
+		if (Number.isNaN(data)) {
+			return {
+				rule: 'type',
+				code: 'INVALID_TYPE',
+				message: `(${data}) is not a ${this.type}`,
+				meta: { expected: this.type, received: typeof data },
+			};
+		}
+
+		return super.validateType(data);
+	}
+
+	override validate(data: any): ValidateFnReturnType {
+		return super.validate(data);
+	}
+}
+
+export { BooleanSchema };
