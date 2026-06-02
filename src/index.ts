a2f8cccc451620cfaa0238875de90d56c40e3634
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-02 17:09:12 +0530

    fix cusom errors for main schemas

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { BooleanSchema } from './schemas/BooleanSchema.js';
 import { NumberSchema } from './schemas/NumberSchema.js';
 import { ObjectSchema } from './schemas/ObjectSchema.js';
 import { StringSchema } from './schemas/StringSchema.js';
+import type { Params } from './types.js';
 
 const v = {
-	string: () => new StringSchema(),
-	number: () => new NumberSchema(),
-	boolean: () => new BooleanSchema(),
+	string: (params?: Params) => new StringSchema([], params),
+	number: (params?: Params) => new NumberSchema([], params),
+	boolean: (params?: Params) => new BooleanSchema([], params),
 	object: (shape: any) => new ObjectSchema(shape),
 };
 
